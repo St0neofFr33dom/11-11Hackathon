8e93c6afa9d386e3b65d388b44634ca6a95a5251
St0neofFr33dom <christophefreeston@googlemail.com>
2022-11-11 13:49:19 +0000

    Made submit form each field changes a state variable, ready to add a submit function

--- front-end/src/componets/PostForm.js
+++ front-end/src/componets/PostForm.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import "../style/postForm.css"
 
 const PostForm = () => {
 
@@ -19,12 +20,38 @@ const PostForm = () => {
             setForm({...form, [name]:value})
         }
 
+        function handleSubmit() {
+            //post request goes here
+        }
+
     return (
-        <div>
-            <label forhtml="name">Name:</label>
-            <input type="text" id="name" name="name" placeholder='Name...' value={form.name} onChange={handleChange}></input>
-            <label forhtml="title">Title:</label>
-            <input type="text" id="title" name="title" placeholder='Title of Post' value={form.title} onChange={handleChange}></input>
+        <div className="postForm">
+        <h2>Submit a Resource</h2>
+            <div className="postFormInput">
+                <label forhtml="name">Name:</label>
+                <input type="text" id="name" name="name" placeholder='Name...' value={form.name} onChange={handleChange}></input>
+            </div>
+            <div className="postFormInput">
+                <label forhtml="title">Title:</label>
+                <input type="text" id="title" name="title" placeholder='Title of Post' value={form.title} onChange={handleChange}></input>
+            </div>
+            <div className="postFormInput">
+            <label forhtml="description">Description:</label>
+            <input type="text" id="description" name="description" placeholder='Brief summary' value={form.description} onChange={handleChange}></input>
+            </div>
+            <div className="postFormInput">
+            <label forhtml="link">Link:</label>
+            <input type="text" id="link" name="link" placeholder='URL of resource' value={form.link} onChange={handleChange}></input>
+            </div>
+            <div className="postFormInput">
+            <label forhtml="imageURL">ImageURL:</label>
+            <input type="text" id="imageURL" name="imageURL" placeholder='URL of a preview image' value={form.imageURL} onChange={handleChange}></input>
+            </div>
+            <div className="postFormInput">
+            <label forhtml="imageAlt">ImageTitle:</label>
+            <input type="text" id="imageAlt" name="imageAlt" placeholder='Brief description of image' value={form.imageAlt} onChange={handleChange}></input>
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
