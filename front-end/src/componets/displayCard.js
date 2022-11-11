1b5224621744b628eb4f3f3bb4e653a09981b1a5
kunalshukla <kun.shukla@gmail.com>
2022-11-11 11:48:35 +0000

    made basic struture of disply card component

--- front-end/src/componets/displayCard.js
+++ front-end/src/componets/displayCard.js
@@ -1,13 +1,20 @@
-import React from 'react'
-
-export const DisplayCard = ({title,description,link,imageURL, imageALT}) => {
-    return (
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <a href={link}>click here!</a>
-            <img src={imageURL} alt={imageALT}/>
-        </div>
-    )
-}
-
+import React from "react";
+import "../style/displayCard.css";
+export const DisplayCard = ({
+  title,
+  description,
+  link,
+  imageURL,
+  imageALT,
+}) => {
+  return (
+    <div className="displayCard">
+      <img src={imageURL} alt={imageALT} />
+      <div className="displayCardBody">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <a href={link}>click here!</a>
+      </div>
+    </div>
+  );
+};
